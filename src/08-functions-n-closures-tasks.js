4e643b6042f61cb7f451a5310b09efcc003de8bb
LeonID <lmatskevich79@gmail.com>
2023-01-03 01:29:13 +0300

    feat: add function partialUsingArguments(fn, ...args1)

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -140,8 +140,11 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function part(...args2) {
+    const arr = [...args1, ...args2];
+    return fn(...arr);
+  };
 }
 
 /**
