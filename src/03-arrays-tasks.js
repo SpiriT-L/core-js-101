2da22b63df5a4193462b5fe209df5f6cafd074fc
LeonID <lmatskevich79@gmail.com>
2023-01-02 22:46:18 +0300

    feat: add function getPositivesCount(arr)

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -300,8 +300,13 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  let counter = 0;
+  arr.map((element) => {
+    if (typeof element === 'number' && element > 0) counter += 1;
+    return element;
+  });
+  return counter;
 }
 
 /**
